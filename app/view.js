cbb5549d368f586d769ef81af1ebff8d4db08401
yannXtrem <yannickmvogo3@gmail.com>
2025-01-30 22:44:29 +0300

    .

--- app/view.js
+++ app/view.js
@@ -47,10 +47,10 @@ class View {
                 <p class="text-sm text-gray-600">${request.description}</p>
                 <p class="text-xs text-gray-500 mt-2">${type}</p>
                 <span class="inline-flex overflow-hidden rounded-md border bg-white shadow-sm">
-                    <button onclick="handleHelpButtonClick(${request.id});" data-id="${request.id}" class="help-button inline-block border-e p-3 text-gray-700 hover:bg-gray-50 focus:relative">
+                    <button data-id="${request.id}" class="help-button inline-block border-e p-3 text-gray-700 hover:bg-gray-50 focus:relative">
                         🤝
                     </button>
-                    <button onclick="handleMuteButtonClick(${request.id});" data-id="${request.id}" class="mute-button inline-block p-3 text-gray-700 hover:bg-gray-50 focus:relative">
+                    <button data-id="${request.id}" class="mute-button inline-block p-3 text-gray-700 hover:bg-gray-50 focus:relative">
                         🔇
                     </button>
                 </span>
