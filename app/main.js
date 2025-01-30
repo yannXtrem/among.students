a6cccdf258c92f86b8fcccae8a4c1e6db9e5a3d6
yannXtrem <62825195+yannXtrem@users.noreply.github.com>
2025-01-30 16:24:04 +0300

    Update main.js : Implemented the Telegram ThemeParam

--- app/main.js
+++ app/main.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Appliquer les couleurs du thème Telegram
+    const themeParams = Telegram.WebApp.themeParams;
+    document.body.style.backgroundColor = themeParams.bg_color || '#ffffff';
+    document.body.style.color = themeParams.text_color || '#000000';
+    
     const model = new Model();
     const view = new View();
     const controller = new Controller(model, view);
